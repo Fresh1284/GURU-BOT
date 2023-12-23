4d53ba8e699e03c497079b3686170fdba138027e
Fresh1284 <154643011+Fresh1284@users.noreply.github.com>
2023-12-23 01:49:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+234 904 699 6030', 'Fresh bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
